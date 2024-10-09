fd586c14b3e775f92f2356743f2ba9e7a99001a5
Tmosco <tmosco_tayo@yahoo.com>
2024-10-09 18:29:06 +0100

    create the card component and CreateProfilePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,14 @@
 
 import React from "react";
 import SignupPage from "./auth/signup/page";
+import CardProfile from "@/components/CardProfile";
+import CreateProfilePage from "./auth/create-profile/page";
 
 export default function Home() {
   return (
     <main className="justify-center flex flex-col w-full items-center">
       <SignupPage />
+     <CreateProfilePage/>
 
     </main>
   );

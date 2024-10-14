923f668cf2f0f871fc310a8180ac1cba3d2565a2
ibkay998 <ibukun46@gmail.com>
2024-10-14 17:28:18 +0100

    renamed

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,3 +1,40 @@
+// "use client";
+// import React, { useState, useEffect } from "react";
+// import Image from "next/image";
+
+// const ImageSlider = ({ images }: any) => {
+//   const [currentIndex, setCurrentIndex] = useState(0);
+
+//   const handleNext = () => {
+//     const isLastSlide = currentIndex === images.length - 1;
+//     const newIndex = isLastSlide ? 0 : currentIndex + 1;
+//     setCurrentIndex(newIndex);
+//   };
+
+//   useEffect(() => {
+//     const slideInterval = setInterval(() => {
+//       handleNext();
+//     }, 2000);
+
+//     // Cleanup interval on component unmount
+//     return () => clearInterval(slideInterval);
+//   }, [images.length, currentIndex]);
+
+//   return (
+//     <div className="relative w-full mx-auto rounded-lg">
+//       <div>
+//         <Image
+//           src={images[currentIndex]}
+//           alt={`Slide ${currentIndex + 1}`}
+//           fill
+//           className="object-cover rounded-lg"
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ImageSlider;
 "use client";
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
@@ -21,13 +58,13 @@ const ImageSlider = ({ images }: any) => {
   }, [images.length, currentIndex]);
 
   return (
-    <div className="relative w-full mx-auto rounded-lg">
-      <div>
+    <div className="relative w-full mx-auto rounded-[32px] overflow-hidden">
+      <div className="relative w-full h-full">
         <Image
           src={images[currentIndex]}
           alt={`Slide ${currentIndex + 1}`}
           fill
-          className="object-cover rounded-lg"
+          className="object-cover"
         />
       </div>
     </div>

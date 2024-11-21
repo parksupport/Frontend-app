e10f4d9e8cb9de02b1fd45f4dddcaed1069a5294
Tmosco <tmosco_tayo@yahoo.com>
2024-11-21 17:12:00 +0100

    done

--- src/components/ModalComponent.tsx
+++ src/components/ModalComponent.tsx
@@ -10,29 +10,35 @@ import {
   Button,
   useDisclosure,
 } from '@chakra-ui/react';
+import SubscriptionPlans from './Subscription';
 
 function ModalComponent({ isOpen, onClose, onOpen, toggleDrawer }) {
 
   return (
     
-    <Modal isOpen={isOpen} onClose={onClose}>
-
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Modal Title</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          <p>Here goes the modal content.</p>
-        </ModalBody>
-
-        <ModalFooter>
-          <Button colorScheme="blue" mr={3} onClick={onClose}>
-            Close
-          </Button>
-          <Button variant="ghost">Secondary Action</Button>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
+<Modal isOpen={isOpen} onClose={onClose} size="full">
+  <ModalOverlay />
+  <ModalContent
+    maxW="100vw"
+    maxH="100vh"
+    height="100vh"
+    borderRadius="0"
+    m="0"
+  >
+    <ModalCloseButton
+      position="absolute"
+      top="16px"
+      left="16px"
+      size="lg"
+      fontSize="24px"
+      width="48px"
+      height="48px"
+    />
+    <ModalBody p="0">
+      <SubscriptionPlans />
+    </ModalBody>
+  </ModalContent>
+</Modal>
   );
 }
 

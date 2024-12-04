f44e9a1310de8effe93b7721ecaa3b52d6a9e8fa
Tmosco <tmosco_tayo@yahoo.com>
2024-12-04 19:12:45 +0100

    overflow add to carprofile

--- src/components/Drawer/DrawerHeader.tsx
+++ src/components/Drawer/DrawerHeader.tsx
@@ -1,5 +1,3 @@
-
-
 import { groteskText, groteskTextMedium } from "@/app/fonts";
 import React, { ReactNode } from "react";
 import { IoArrowBack } from "react-icons/io5";
@@ -18,11 +16,12 @@ const DrawerHeader = ({
   className,
 }: DrawerHeaderProps) => {
   return (
-    <div className={`flex  pt-[60px] items-center justify-between px-2 md:px-0`}>
+    <div
+      className={`flex  pt-[60px] items-center justify-between px-2 md:px-0`}
+    >
       <div className="flex  items-start md:items-center ">
-
         <div className="cursor-pointer" onClick={toggleDrawer}>
-          <IoArrowBack className="w-8 h-8 md:w-8 md:h-8" color="#000000"/>
+          <IoArrowBack className="w-8 h-8 md:w-8 md:h-8" color="#000000" />
         </div>
         <div className="flex flex-col items-center">
           <h1
@@ -40,30 +39,7 @@ const DrawerHeader = ({
         </div>
       </div>
     </div>
-//     <div className={`flex pt-[60px] items-center justify-between px-2 md:px-0`}>
-//   <div className="flex items-center md:space-x-4">
-//     <div className="cursor-pointer" onClick={toggleDrawer}>
-//       <IoArrowBack className="w-8 h-8 md:w-8 md:h-8" color="#000000" />
-//     </div>
-//     <div className="flex flex-col ">
-//       <h1
-//         className={`text-[24px] leading-none md:text-[32px] text-center text-[#000000] lg:text-[32px] ${className} ${groteskTextMedium.className}`}
-//       >
-//         {title}
-//       </h1>
-//       {subTitle && (
-//         <p
-//           className={`text-[#667185] text-center leading-none ${groteskText.className}`}
-//         >
-//           {subTitle}
-//         </p>
-//       )}
-//     </div>
-//   </div>
-// </div>
-
   );
 };
 
 export default DrawerHeader;
-

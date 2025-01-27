5c8cb0aa9479a0c08a9f458f02abb2b6b4e67b84
Tmosco <tmosco_tayo@yahoo.com>
2025-01-27 12:50:31 +0100

    notification spinner

--- src/components/card/DashBoardNotification.tsx
+++ src/components/card/DashBoardNotification.tsx
@@ -12,7 +12,7 @@ import {
 import { useFetchNotifications } from "@/hooks/queries/notifications";
 import useNotifications from "@/hooks/useNotification";
 import Image from "next/image";
-import { Button } from "@chakra-ui/react";
+import { Button, Spinner } from "@chakra-ui/react";
 
 interface NotificationProps {
   id: number;
@@ -45,7 +45,6 @@ const DashboardNotifications = ({ openNotificationsTable, isDrawer }) => {
     totalNotifications,
   } = useNotifications(notificationsData, 7);
 
-
   const isMobile = useIsMobile();
 
   function getNotificationMessage({
@@ -66,73 +65,83 @@ const DashboardNotifications = ({ openNotificationsTable, isDrawer }) => {
     return null;
   }
 
-  return notificationsData?.length === 0 ? (
-    <div className="max-w-[396px] min-h-[340px] w-full lg:max-w-[680px] bg-white rounded-[1.25rem] py-2 px-4 md:py-2 md:px-6 lg:px-8 flex items-center justify-center flex-col">
-      <div className="flex flex-col items-center justify-center">
-        <div className={`${groteskTextMedium.className} text-[32px]`}>
-          No Notification Yet
+  return (
+    <>
+      {isLoading ? (
+        <div className="max-w-[396px] min-h-[340px] w-full lg:max-w-[680px] bg-white rounded-[1.25rem] py-2 px-4 md:py-2 md:px-6 lg:px-8 flex items-center justify-center flex-col">
+          <Spinner size="lg" color="blue" />
         </div>
-        <Image
-          src={require(`@/assets/images/notification_emptyState.png`)}
-          alt=""
-          sizes="width: 200px"
+      ) : notificationsData?.length === 0 ? (
+        <div className="max-w-[396px] min-h-[340px] w-full lg:max-w-[680px] bg-white rounded-[1.25rem] py-2 px-4 md:py-2 md:px-6 lg:px-8 flex items-center justify-center flex-col">
+          <div className="flex flex-col items-center justify-center">
+            <div className={`${groteskTextMedium.className} text-[32px]`}>
+              No Notification Yet
+            </div>
+            <Image
+              src={require(`@/assets/images/notification_emptyState.png`)}
+              alt=""
+              sizes="width: 200px"
+            />
+          </div>
+        </div>
+      ) : isMobile ? (
+        <MobileViewNotification
+          openNotificationsDrawer={openNotificationsTable}
+          isDrawer={isDrawer}
+          handleSelectAll={handleSelectAll}
+          selectAll={isAllSelected}
+          handleCheckboxChange={handleCheckboxChange}
+          currentNotifications={currentNotifications}
+          totalPages={totalPages}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          handleNext={goToNextPage}
+          handlePrevious={goToPreviousPage}
+          cardNotificationClick={openNotificationsTable}
+          notificationStateMessage={getNotificationMessage({
+            isLoading,
+            isError,
+            error,
+          })}
         />
-      </div>
-    </div>
-  ) : isMobile ? (
-    <MobileViewNotification
-      openNotificationsDrawer={openNotificationsTable}
-      isDrawer={isDrawer}
-      handleSelectAll={handleSelectAll}
-      selectAll={isAllSelected}
-      handleCheckboxChange={handleCheckboxChange}
-      currentNotifications={currentNotifications}
-      totalPages={totalPages}
-      currentPage={currentPage}
-      setCurrentPage={setCurrentPage}
-      handleNext={goToNextPage}
-      handlePrevious={goToPreviousPage}
-      cardNotificationClick={openNotificationsTable}
-      notificationStateMessage={getNotificationMessage({
-        isLoading,
-        isError,
-        error,
-      })}
-    />
-  ) : (
-    <div className="hidden md:block bg-white p-4 rounded-[20px] border border-gray-200 w-full">
-      <div className="flex justify-between py-[12px]">
-        <h2
-          className={` ${groteskTextMedium.className} text-[32px] leading-none text-black `}
-        >
-          Notifications
-        </h2>
-        <div className="p-1 cursor-pointer" onClick={openNotificationsTable}>
-          <MoveDiagonal size={24} />
+      ) : (
+        <div className="hidden md:block bg-white p-4 rounded-[20px] border border-gray-200 w-full">
+          <div className="flex justify-between py-[12px]">
+            <h2
+              className={`${groteskTextMedium.className} text-[32px] leading-none text-black`}
+            >
+              Notifications
+            </h2>
+            <div
+              className="p-1 cursor-pointer"
+              onClick={openNotificationsTable}
+            >
+              <MoveDiagonal size={24} />
+            </div>
+          </div>
+          <DesktopViewNotification
+            isDrawer={isDrawer}
+            handleSelectAll={handleSelectAll}
+            selectAll={isAllSelected}
+            currentNotifications={currentNotifications}
+            totalPages={totalPages}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            handleNext={goToNextPage}
+            handlePrevious={goToPreviousPage}
+            itemsPerPage={itemsPerPage}
+            totalNotifications={totalNotifications}
+            textMaxLenght={40}
+            cardNotificationClick={openNotificationsTable}
+            notificationStateMessage={getNotificationMessage({
+              isLoading,
+              isError,
+              error,
+            })}
+          />
         </div>
-      </div>
-      <DesktopViewNotification
-        isDrawer={isDrawer}
-        handleSelectAll={handleSelectAll}
-        selectAll={isAllSelected}
-        currentNotifications={currentNotifications}
-        totalPages={totalPages}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-        handleNext={goToNextPage}
-        handlePrevious={goToPreviousPage}
-        itemsPerPage={itemsPerPage}
-        totalNotifications={totalNotifications}
-        textMaxLenght={40}
-        cardNotificationClick={openNotificationsTable}
-        notificationStateMessage={getNotificationMessage({
-          isLoading,
-          isError,
-          error,
-        })}
-      />
-    </div>
+      )}
+    </>
   );
 };
-
 export default DashboardNotifications;
